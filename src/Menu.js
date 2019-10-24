081bd8ae92f1b679355e903f9a37df65ce4cdff3
RIZKALLAH <rizkallah.mansouri@capgemini.com>
2019-10-24 16:07:22 +0100

    [FIX] ajustement du header et footer

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import logo from '../src/images/benchup-logo.png';
@@ -8,27 +8,24 @@ function Menu() {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Nav>
-                <Navbar.Brand href="/home"><img src={logo} alt="Logo" height="50" width="50"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                </Nav>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        {/* <Nav.Link href="projets"><Link to="/">Home</Link></Nav.Link> */}
-                        {/* <Nav.Link href="staffing"><Link to="/newProject">Add project</Link></Nav.Link> */}
-                        <Nav.Link href="digicampiste"><Link to="/digicampiste">Digicampistes</Link></Nav.Link>
-                        <Nav.Link href="projet"><Link to="/projet">Projets</Link></Nav.Link>
-                        <Nav.Link href="contributeur"><Link to="/contributeur">Contributeurs</Link></Nav.Link>
-                        </Nav>
-                        <Nav>
-                        <Nav.Link href="digicampiste"><Link to="/logout">Se deconnecter</Link></Nav.Link>
-                        </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+        <Navbar.Collapse className="justify-content-right">
+          <Navbar.Brand href="/home"><img src={logo} alt="Logo" height="50" width="50" /></Navbar.Brand>
+        </Navbar.Collapse>
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
+          {/* <Nav.Link href="projets"><Link to="/">Home</Link></Nav.Link> */}
+          {/* <Nav.Link href="staffing"><Link to="/newProject">Add project</Link></Nav.Link> */}
+          <Nav.Item><Nav.Link href="digicampiste"><Link to="/digicampiste">Digicampistes</Link></Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="projet"><Link to="/projet">Projets</Link></Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="contributeur"><Link to="/contributeur">Contributeurs</Link></Nav.Link></Nav.Item>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav.Link href="digicampiste"><Link to="/logout">Se deconnecter</Link></Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
-    
+
   );
-  
+
 }
 
 export default Menu
